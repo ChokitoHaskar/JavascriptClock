0dac7092e6f2cf7482b283ec6dd4df388db18ee4
BukanChoki <55580042+ChokitoHaskar@users.noreply.github.com>
2022-09-22 14:16:52 +0700

    add Analog clock

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-setInterval(timer, 1000);
+$("#clockAnalog").hide();
+$("#clockDigital").show();
 
 const days = [
   "Sunday",
@@ -25,18 +26,67 @@ const months = [
   "December",
 ];
 
-function timer() {
+$("#setDigital").click(function () {
+  $("#clockAnalog").hide();
+  $("#clockDigital").show();
+});
+
+$("#setAnalog").click(function () {
+  $("#clockDigital").hide();
+  $("#clockAnalog").show();
+});
+
+window.timeInterval = setInterval(setTime, 1000);
+
+function setTime() {
   const time = new Date();
-  $("#clock").text(
-    time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds()
-  );
+
+  timeDigital(time);
+  timeAnalog(time);
+
   $("#date").text(
     days[time.getDay()] +
-      "," +
+      ", " +
       time.getDate() +
       " " +
       months[time.getMonth()] +
       " " +
       time.getFullYear()
   );
+}
+
+function timeAnalog(time) {
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
+
+  let secondsDegrees = (seconds / 60) * 360 + 90;
+  let minutesDegrees = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
+  let hourDegrees = (hours / 12) * 360 + (minutes / 60) * 30 + 90;
+
+  $("#handSeconds").css({ transform: "rotate(" + secondsDegrees + "deg)" });
+  $("#handMinutes").css({ transform: "rotate(" + minutesDegrees + "deg)" });
+  $("#handHours").css({ transform: "rotate(" + hourDegrees + "deg)" });
+}
+
+function timeDigital(time) {
+  let hours = time.getHours().toString();
+  let minutes = time.getMinutes().toString();
+  let seconds = time.getSeconds().toString();
+
+  if (hours.length == 1) {
+    hours = "0" + hours;
+  }
+  if (minutes.length == 1) {
+    minutes = "0" + minutes;
+  }
+  if (seconds.length == 1) {
+    seconds = "0" + seconds;
+  }
+
+  $("#clockDigital").text(hours + ":" + minutes + ":" + seconds);
+
+  if (!time) {
+    return;
+  }
 }
